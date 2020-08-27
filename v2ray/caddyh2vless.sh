f32448e7e1cf8352324fd65579f49397f913fcdf
Mcl <mixool0204@gmail.com>
2020-08-27 18:45:21 +0800

    Update caddyh2vless.sh

--- v2ray/caddyh2vless.sh
+++ v2ray/caddyh2vless.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 # Usage:  debian 9/10 one_key for caddy2 v2ray
 # install: bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/v2ray/caddyh2vless.sh) my.domain.com
-# uninstall: systemctl disable caddy; systemctl stop caddy; rm -rf /etc/caddy/Caddyfile /lib/systemd/system/caddy.service; bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove; rm -rf /usr/local/etc/v2ray /var/log/v2ray
+# uninstall: apt purge caddy -y; systemctl disable caddy; systemctl stop caddy; rm -rf /etc/caddy /lib/systemd/system/caddy.service; bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove; rm -rf /usr/local/etc/v2ray /var/log/v2ray
 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT
@@ -21,7 +21,7 @@ caddyURL="$(wget -qO-  https://api.github.com/repos/caddyserver/caddy/releases |
 wget -O $TMPFILE $caddyURL && dpkg -i $TMPFILE
 wget --no-check-certificate -O /lib/systemd/system/caddy.service https://raw.githubusercontent.com/caddyserver/dist/master/init/caddy.service
 wget --no-check-certificate -O /usr/share/caddy/index.html https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
-sed -i -e "s/User=caddy$/User=root/g" -e "/Group=caddy$/d" -e "s/caddy\/Caddyfile$/caddy\/Caddyfile\.json/g" -e "s/^LimitNPROC=.*/LimitNPROC=51200/g" /lib/systemd/system/caddy.service
+sed -i -e "s/User=caddy$/User=root/g" -e "/Group=caddy$/d" -e "s/^LimitNPROC=.*/LimitNPROC=51200/g" /lib/systemd/system/caddy.service
 
 # install v2ray; update geoip.dat && geosite.dat
 bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
@@ -59,7 +59,7 @@ cat <<EOF >/usr/local/etc/v2ray/config.json
             "settings": {"clients": [{"id": "$v2my_uuid"}],"decryption": "none"},
             "streamSettings": {"network": "h2","httpSettings": {"path": "/$v2my_path","host": ["$domain"]}}
         }
-    ]
+    ],
 	
     "outbounds": 
     [
@@ -79,6 +79,7 @@ cat <<EOF >/usr/local/etc/v2ray/config.json
 EOF
 
 # systemctl service info
+systemctl daemon-reload
 echo; echo $(date) caddy status:
 systemctl enable caddy && systemctl restart caddy && sleep 1 && systemctl status caddy | more | grep -A 2 "caddy.service"
 echo; echo $(date) v2ray status:
