5882c5eb25c1b2f2694d06b127ddca3cd3210be7
Mcl <mixool0204@gmail.com>
2020-09-24 15:54:53 +0800

    Update trojan-go-acme-cloudflare.sh

--- trojan-go/trojan-go-acme-cloudflare.sh
+++ trojan-go/trojan-go-acme-cloudflare.sh
@@ -3,23 +3,25 @@
 # Usage: bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/trojan-go/trojan-go-acme-cloudflare.sh) my.domain.com CF_Key CF_Email
 ## one key install trojan-go with acme and cloudflareApi
 ## Uninstall: /root/.acme.sh/acme.sh --uninstall; systemctl stop trojan-go.service; systemctl disable trojan-go.service; rm -rf /etc/systemd/system/trojan-go.service /usr/bin/trojan-go /etc/trojan-go
+### 如不启用cloudflare cdn，脚本运行前需要关闭cloudflare的小云朵
+### 如需启用cloudflare cdn，脚本运行前需要打开cloudflare的小云朵，并在脚本运行完成后修改两端的config.json文件中websocket项的false改为true
+
 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT
 TMPFILE=$(mktemp) || exit 1
 
-######## 脚本需要传入三个参数： 域名,Cloudflare账户的GobalAPI,Cloudflare账户的Email
+######## 脚本需要传入三个参数： 域名,Cloudflare账户的GobalAPI,Cloudflare账户的Email,
 [[ $# != 3 ]] && echo Err !!! Useage: bash this_script.sh my.domain.com CF_Key CF_Email && exit 1
 domain="$1"
 export CF_Key="$2"
 export CF_Email="$3"
-########
 
 # install acme.sh
 apt install socat -y
 curl https://get.acme.sh | sh
 source  ~/.bashrc
-/root/.acme.sh/acme.sh --issue --dns dns_cf --keylength ec-256 -d $domain
+/root/.acme.sh/acme.sh --issue --dns dns_cf --keylength ec-256 -d $domain --force
 rm -rf /etc/trojan-go; mkdir -p /etc/trojan-go
 /root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchain-file /etc/trojan-go/trojan-go.crt --key-file /etc/trojan-go/trojan-go.key --reloadcmd "service trojan-go restart"
 
@@ -32,6 +34,7 @@ sed -i -e "s/User=nobody$/User=root/g" /etc/systemd/system/trojan-go.service
 
 # config trojan-go
 password="$(tr -dc 'a-z0-9A-Z' </dev/urandom | head -c 16)"
+webspath="$(tr -dc 'a-z0-9A-Z' </dev/urandom | head -c 16)"
 
 cat <<EOF > /etc/trojan-go/config.json
 {
@@ -46,7 +49,11 @@ cat <<EOF > /etc/trojan-go/config.json
     "ssl": {
         "cert": "/etc/trojan-go/trojan-go.crt",
         "key": "/etc/trojan-go/trojan-go.key"
-    }
+    },
+    "websocket": {
+		"enabled": false,
+		"path": "/$webspath"
+	}
 }
 EOF
 
@@ -63,7 +70,11 @@ cat <<EOF >$TMPFILE
     "remote_port": 443,
     "password": [
         "$password"
-    ]
+    ],
+    "websocket": {
+    "enabled": false,
+    "path": "/$webspath"
+	}
 }
 EOF
 
