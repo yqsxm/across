be900224f757b61ade7b837d7df00a1813089387
Mcl <mixool0204@gmail.com>
2020-10-07 01:18:32 +0800

    Create exfly.sh

---
+++ dockershc/exfly.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+## 用于https://github.com/mixool/dockershc项目安装运行v2ray的脚本
+
+if [[ "$(command -v workerone)" == "" ]]; then
+    # install and rename
+    wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip -
+    chmod +x /v2ray /v2ctl && mv /v2ray /usr/bin/workerone && mv /v2ctl /usr/bin/v2ctl && mv /geosite.dat /usr/bin/geosite.dat && mv /geoip.dat /usr/bin/geoip.dat
+    # config
+    cat <<EOF >/usr/bin/config.json
+{
+    "inbounds": 
+    [
+        {
+            "port": 3000,"protocol": "vless",
+            "settings": {"clients": [{"id": "8f91b6a0-e8ee-11ea-adc1-0242ac120002"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "/vlesspath"}}
+        }
+    ],
+    
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}
+        ]
+    }
+}
+EOF
+else
+    # start 
+    workerone -config /usr/bin/config.json >/dev/null 2>&1
+fi
