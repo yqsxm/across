aa8528d8151dff32b3e836e05db40583e581398e
Mcl <mixool0204@gmail.com>
2020-10-15 11:15:27 +0800

    Update exfly.sh

--- dockershc/exfly.sh
+++ dockershc/exfly.sh
@@ -11,7 +11,7 @@ if [[ "$(command -v workerone)" == "" ]]; then
     "inbounds": 
     [
         {
-            "port": 3000,"protocol": "vless",
+            "port": "env:PORT","listen": "0.0.0.0","protocol": "vless",
             "settings": {"clients": [{"id": "8f91b6a0-e8ee-11ea-adc1-0242ac120002"}],"decryption": "none"},
             "streamSettings": {"network": "ws","wsSettings": {"path": "/vlesspath"}}
         }
